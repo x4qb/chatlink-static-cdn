019dec1d4d14dfdbaed4849ee47e21d8425328fe
solo <113215526+s0lotime@users.noreply.github.com>
2025-05-10 16:00:58 -0400

    Update chatlink.js

--- js/chatlink.js
+++ js/chatlink.js
@@ -77,9 +77,10 @@ async function connectWebSocket(roomName) {
   socket.onclose = () => {
     console.log('Connection to Chatlink expired. Reload');
     document.getElementById("messageInput").disabled = true;
-    document.getElementById('imageInput').disabled = true;
-    document.getElementById('messageInput').style.cursor = "not-allowed";
-    document.getElementById('imageInput').style.cursor = "not-allowed";
+    document.getElementById("imageInput").disabled = true;
+    document.getElementById("messageInput").placeholder = "Chatlink connection closed. Reload the page to continue talking!"
+    document.getElementById("messageInput").style.cursor = "not-allowed";
+    document.getElementById("imageInput").style.cursor = "not-allowed";
   };
 }
 
